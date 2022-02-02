14b0dde8eb922df729866320b353c826308aa719
DevGomes78 <gomes.amilson@gmail.com>
2022-02-02 12:04:49 -0300

    alterado

--- lib/screens/busca_cep.dart
+++ lib/screens/busca_cep.dart
@@ -13,51 +13,83 @@ class BuscaCep extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Busca Cep'),
+        title: Text(
+          'Busca Cep',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.blueAccent,
+        elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              width: 20,
-            ),
-            //Widget SearchBar
-            SearchBar(context),
-            SizedBox(
-              height: 40,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Center(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: 20,
+              ),
+              //Widget SearchBar
+              SearchBar(context),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Endereço',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 40,
+                width: 365,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xffBFCEECFF),
+                ),
+                alignment: Alignment.center,
                 child: Text(
                   provider.endereco.toString(),
                   style: TextStyle(fontSize: 18, color: Colors.black),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Center(
-                child: Text(
-                  provider.bairro.toString(),
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 40,
+                    width: 157,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xffBFCEECFF),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      provider.bairro.toString(),
+                      style: TextStyle(fontSize: 18, color: Colors.black),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 40,
+                      width: 198,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xffBFCEECFF)),
+                      alignment: Alignment.center,
+                      child: Text(
+                        provider.localidade.toString(),
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Center(
-                child: Text(
-                  provider.localidade.toString(),
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
